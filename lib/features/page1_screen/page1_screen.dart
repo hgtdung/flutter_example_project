71534fbef47c0713297c9054e29878591ac3aaa5
hoangdung <hgtdung@gmail.com>
2025-03-03 20:01:06 +0700

    init

--- lib/features/page1_screen/page1_screen.dart
+++ lib/features/page1_screen/page1_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_example_project/di/service_locator.dart';
 import 'package:flutter_example_project/features/page1_screen/page1_vm.dart';
 import 'package:flutter_example_project/utils/utils.dart';
 import 'package:provider/provider.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
+
 
 class Page1Screen extends StatefulWidget {
   const Page1Screen({super.key});
@@ -36,6 +41,10 @@ class _Page1ScreenState extends State<Page1Screen> {
                     return Column(
                       children: [
                         Text(viewModel.randomText),
+                        ElevatedButton(onPressed: () {
+                          initSqlite3();
+                        },
+                        child: Text("Test Sqlite3"))
                       ],
                     );
                   }, child: Text("Child of consumer"),)
@@ -50,5 +59,54 @@ class _Page1ScreenState extends State<Page1Screen> {
   void dispose() {
     page1ScreenVM.dispose();
     super.dispose();
+  }
+
+  void initSqlite3() {
+    print('Using sqlite3 ${sqlite3.version}');
+
+    // Create a new in-memory database. To use a database backed by a file, you
+    // can replace this with sqlite3.open(yourFilePath).
+    final db = sqlite3.openInMemory();
+
+    // Create a table and insert some data
+    db.execute('''
+    CREATE TABLE artists (
+      id INTEGER NOT NULL PRIMARY KEY,
+      name TEXT NOT NULL
+    );
+  ''');
+
+    // Prepare a statement to run it multiple times:
+    final stmt = db.prepare('INSERT INTO artists (name) VALUES (?)');
+    stmt
+      ..execute(['The Beatles'])
+      ..execute(['Led Zeppelin'])
+      ..execute(['The Who'])
+      ..execute(['Nirvana']);
+
+    // Dispose a statement when you don't need it anymore to clean up resources.
+    stmt.dispose();
+
+    // You can run select statements with PreparedStatement.select, or directly
+    // on the database:
+    final ResultSet resultSet =
+    db.select('SELECT * FROM artists WHERE name LIKE ?', ['The %']);
+
+    // You can iterate on the result set in multiple ways to retrieve Row objects
+    // one by one.
+    for (final  row in resultSet) {
+      print('Artist[id: ${row['id']}, name: ${row['name']}]');
+    }
+
+    // Register a custom function we can invoke from sql:
+    db.createFunction(
+      functionName: 'dart_version',
+      argumentCount: const AllowedArgumentCount(0),
+      function: (args) => Platform.version,
+    );
+    print(db.select('SELECT dart_version()'));
+
+    // Don't forget to dispose the database to avoid memory leaks
+    db.dispose();
   }
 }
